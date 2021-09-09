2f69e9c9d288fdd7b8fd3491931bfb107386e4ec
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-09-08 19:48:36 -0600

    placeholder

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,16 @@
 import logo from '../pics/logo.png';
 import './App.css';
-import LocateDog from '../LocateDog/LocateDog'
+import LocateDog from '../LocateDog/LocateDog';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-         <h1>
-        Find a Hot Dog near you!
-      </h1>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>Find a Hot Dog near you!</h1>
       </header>
-      <LocateDog />
+      <p>This is where the search box and results go</p>
+      {/* <LocateDog /> */}
     </div>
   );
 }
