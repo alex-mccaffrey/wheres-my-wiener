8be690c6db2d78dc8fa774834e66fe882549e3cb
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-09-10 09:17:09 -0600

    minor cleanup

--- src/App/App.js
+++ src/App/App.js
@@ -9,7 +9,13 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <h1>Find a Hot Dog near you!</h1>
       </header>
-      <p>This is where the search box and results go</p>
+      <p>This is where the search box</p>
+      <ul>
+        <li>Result 1</li>
+        <li>Result 2</li>
+        <li>Result 3</li>
+        <li>Result 4</li>
+      </ul>
       {/* <LocateDog /> */}
     </div>
   );
